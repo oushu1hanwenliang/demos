a62dd7adaa7e3e3d2994019553935787a8468a2c
oushu1hanwenliang <han.wenliang@oushu.com>
2023-05-26 14:24:19 +0800

    rebase

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"log"
-
 	"github.com/vladimirvivien/gowfs"
 )
 
@@ -19,10 +17,4 @@ func main() {
 }
 
 func createTestDir(fs *gowfs.FileSystem, hdfsPath string) {
-	path := gowfs.Path{Name: hdfsPath}
-	ok, err := fs.MkDirs(path, 0744)
-	if err != nil || !ok {
-		log.Fatal("Unable to create test directory ", hdfsPath, ":", err)
-	}
-	log.Println("HDFS Path ", path.Name, " created.")
 }
