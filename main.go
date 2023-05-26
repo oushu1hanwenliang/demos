6fcad7c6c1a519dce3bf08eaa4af763372fc10c1
oushu1hanwenliang <han.wenliang@oushu.com>
2023-05-26 15:30:03 +0800

    ree

--- main.go
+++ main.go
@@ -20,7 +20,6 @@ func createTestDir(fs *gowfs.FileSystem, hdfsPath string) {
 }
 func createTestDi(fs *gowfs.FileSystem, hdfsPath string) {
 	path := gowfs.Path{Name: hdfsPath}
-	ok, err := fs.MkDirs(path, 0744)
 	if err != nil || !ok {
 		log.Fatal("Unable to create test directory ", hdfsPath, ":", err)
 	}
@@ -31,16 +30,8 @@ if __name__ == '__main__':
     # unittest.main()
 
     suite = unittest.TestSuite()
-    # 单个执行测试用例
-    suite.addTest(HdfsApp('test_file_status'))
-    runner = unittest.TextTestRunner()
     runner.run(suite)
 
 if __name__ == '__main__':
     # unittest.main()
-
-    suite = unittest.TestSuite()
-    # 单个执行测试用例
-    suite.addTest(HdfsApp('test_file_status'))
-    runner = unittest.TextTestRunner()
     runner.run(suite)
