ef7178662d4229e22bba6d1cef803a07147bb7cb
oushu1hanwenliang <han.wenliang@oushu.com>
2023-05-26 14:42:30 +0800

    resss main

--- main.go
+++ main.go
@@ -18,3 +18,21 @@ func main() {
 
 func createTestDir(fs *gowfs.FileSystem, hdfsPath string) {
 }
+
+if __name__ == '__main__':
+    # unittest.main()
+
+    suite = unittest.TestSuite()
+    # 单个执行测试用例
+    suite.addTest(HdfsApp('test_file_status'))
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+
+if __name__ == '__main__':
+    # unittest.main()
+
+    suite = unittest.TestSuite()
+    # 单个执行测试用例
+    suite.addTest(HdfsApp('test_file_status'))
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
