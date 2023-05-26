d8c4421602237b876125fad9e8a2807b5e4c6823
oushu1hanwenliang <han.wenliang@oushu.com>
2023-05-26 14:18:44 +0800

    resss main

--- main.go
+++ main.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/vladimirvivien/gowfs"
 )
 
 func main() {
-	fs, err := gowfs.NewFileSystem(gowfs.Configuration{Addr: "http://43.138.23.161:9000", User: "hadoop"})
-	if err != nil {
-		log.Fatal(err)
-	}
-	checksum, err := fs.GetFileChecksum(gowfs.Path{Name: "/user/kafka/hello.txt"})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(checksum)
-
-	createTestDir(fs, "/user/kafka/hello1.txt")
+	return
 }
 
 func createTestDir(fs *gowfs.FileSystem, hdfsPath string) {
